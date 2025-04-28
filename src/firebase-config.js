efa5c5ca86159466be5cc31c953921bd34da986e
Geddam-Gowridhar <geddam.gowridhar@sasi.ac.in>
2025-04-28 23:28:53 +0530

    Initial Firebase setup

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,16 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA6WgJMH6PgA9lr-EMVUIMwISqhra97NpA",
+  authDomain: "chatapp-5efd4.firebaseapp.com",
+  databaseURL: "https://chatapp-5efd4-default-rtdb.firebaseio.com",
+  projectId: "chatapp-5efd4",
+  storageBucket: "chatapp-5efd4.firebasestorage.app",
+  messagingSenderId: "920573589411",
+  appId: "1:920573589411:web:bc7b4f1c7b94cd9bfa8c16",
+  measurementId: "G-0P762496P1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
